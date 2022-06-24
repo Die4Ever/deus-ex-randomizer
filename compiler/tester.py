df11e456012b0e432c05dc08dc829da1965a0030
Die4Ever <die4ever2005@gmail.com>
2022-06-24 17:26:04 -0500

    tester.py read from Default.ini instead of DeusEx.ini

--- compiler/tester.py
+++ compiler/tester.py
@@ -6,9 +6,9 @@ def runAutomatedTests(out, package):
     rc = False
     cleanup(out)
 
-    if exists(out + '/System/DeusEx.ini'):
-        # copy DeusEx.ini to test.ini, change [Engine.Engine] DefaultServerGame to =DeusEx.DXRandoTests
-        f = open (out + '/System/DeusEx.ini','r')
+    if exists(out + '/System/Default.ini'):
+        # copy Default.ini (defaults for DeusEx.ini) to test.ini, change [Engine.Engine] DefaultServerGame to =DeusEx.DXRandoTests
+        f = open (out + '/System/Default.ini','r')
         lines = f.readlines()
         f.close()
 
