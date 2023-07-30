ab12801a3a1a08435704e3fc751e32fb25512ba5
Die4Ever <30947252+Die4Ever@users.noreply.github.com>
2023-07-29 19:09:44 -0500

    Installer mkdir DXRandoSave with parents

--- installer/Install/Install.py
+++ installer/Install/Install.py
@@ -102,10 +102,10 @@ def InstallVanilla(system:Path, settings:dict, speedupfix:bool):
 
     if kentie:
         configs_dest = Path.home() / 'Documents' / 'Deus Ex' / 'System'
-        Mkdir(configs_dest.parent /'SaveDXRando', exist_ok=True)
+        Mkdir(configs_dest.parent /'SaveDXRando', exist_ok=True, parents=True)
     else:
         configs_dest = system
-        Mkdir(system.parent /'SaveDXRando', exist_ok=True)
+        Mkdir(system.parent /'SaveDXRando', exist_ok=True, parents=True)
     DXRandoini = configs_dest / (exename+'.ini')
     Mkdir(DXRandoini.parent, parents=True, exist_ok=True)
 
