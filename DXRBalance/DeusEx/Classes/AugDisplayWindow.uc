Die4Ever/deus-ex-randomizer

commit d2ae8c4533dea99efc96c9a911ad524aba28042b

Move some Hover Hint logic out of AugDisplayWindow into functions in DXRHoverHint (#606)

DXRBalance/DeusEx/Classes/AugDisplayWindow.uc
@@ -235,7 +235,6 @@ function DXRHoverHint TraceHoverHint(float checkDist, out vector HitLocation)
 {
     local Actor target;
     local Vector HitLoc;
-    local bool destroyed;
     local DXRHoverHint hoverHint;
 
     target = TraceActorType(class'DXRHoverHint',checkDist);
@@ -243,17 +242,12 @@ function DXRHoverHint TraceHoverHint(float checkDist, out vector HitLocation)
     hoverHint = DXRHoverHint(target);
     if (hoverHint!=None)
     {
-        //Check if the attached target has been destroyed
-        destroyed=False;
-        if (hoverHint.attached){
-            if (#var(DeusExPrefix)Mover(hoverHint.target)!=None){
-                destroyed = #var(DeusExPrefix)Mover(hoverHint.target).bDestroyed;
-            } else {
-                destroyed = (hoverHint.target==None);
-            }
+        if (hoverHint.ShouldSelfDestruct()){
+            hoverHint.Destroy();
+            return None;
         }
-        if (destroyed==True){
-            target.Destroy();
+
+        if (hoverHint.ShouldDisplay()==False){
             return None;
         }
 

DXRando/DeusEx/Classes/DXRHoverHint.uc
@@ -31,6 +31,25 @@ static function DXRHoverHint Create(Actor a, String hint, vector loc, float rad,
     return hoverHint;
 }
 
+function bool ShouldDisplay()
+{
+    return True;
+}
+
+function bool ShouldSelfDestruct()
+{
+    //Check if the attached target has been destroyed
+    if (attached){
+        if (#var(DeusExPrefix)Mover(target)!=None){
+            return #var(DeusExPrefix)Mover(target).bDestroyed;
+        } else {
+            return (target==None);
+        }
+    }
+
+    return False;
+}
+
 defaultproperties
 {
      bCollideActors=True

commit ccee7a379e58084e0e1d2e2aa892eb5399dcc3a0

Fix teleporter destinations being shown for invisible teleporters (the return ones).  This had been broken in #606

DXRBalance/DeusEx/Classes/AugDisplayWindow.uc
@@ -220,7 +220,7 @@ function #var(prefix)Teleporter TraceTeleporter(float checkDist, out vector HitL
 	local Actor target;
 	local Vector HitLoc, HitNormal, StartTrace, EndTrace;
 
-	target = TraceActorType(class'#var(prefix)Teleporter',checkDist);
+	target = TraceActorType(class'#var(prefix)Teleporter',checkDist, false);
 
     if (#var(prefix)Teleporter(target)!=None)
     {
@@ -238,7 +238,7 @@ function DXRHoverHint TraceHoverHint(float checkDist, out vector HitLocation)
     local DXRHoverHint hoverHint;
     local float dist;
 
-    target = TraceActorType(class'DXRHoverHint',checkDist);
+    target = TraceActorType(class'DXRHoverHint',checkDist, true);
 
     hoverHint = DXRHoverHint(target);
     if (hoverHint!=None)
@@ -260,7 +260,7 @@ function DXRHoverHint TraceHoverHint(float checkDist, out vector HitLocation)
     return None;
 }
 
-function Actor TraceActorType(class<Actor> actType,float checkDist)
+function Actor TraceActorType(class<Actor> actType, float checkDist, bool findHidden)
 {
     local Actor target;
     local Vector HitLoc, HitNormal, StartTrace, EndTrace;
@@ -281,8 +281,12 @@ function Actor TraceActorType(class<Actor> actType,float checkDist)
            continue;
         } else if (Player.IsFrobbable(target)){
             return None;
-        } else if (target.bHidden && !target.IsA(actType.name)){
-            continue;
+        } else if (target.bHidden){
+            if (findHidden && target.IsA(actType.name)){
+                break;
+            } else {
+                continue;
+            }
         } else {
             break;
         }