7f236b57c643452b642c39d05722dd8854c57e4c
Die4Ever <die4ever2005@gmail.com>
2025-05-13 20:03:48 -0500

    installer separate exe moved to Advanced settings

--- src/installer/GUI/InstallerWindow.py
+++ src/installer/GUI/InstallerWindow.py
@@ -273,7 +273,7 @@ class InstallerWindow(GUIBase):
         self.globalsettings['shortcuts'] = BooleanVar(master=self.frame, value=True)
         self.shortcuts = Checkbutton(self.frame, text="Create shortcuts for new exe files", variable=self.globalsettings['shortcuts'])
         Hovertip(self.shortcuts, "Create shortcuts on desktop and start menu for newly created exe files.\nOnly applies when creating a new separate exe.")
-        self.setgrid(self.shortcuts, True, column=1,row=self.row, sticky='SW', padx=pad, pady=pad)
+        self.setgrid(self.shortcuts, advanced=True, column=1,row=self.row, sticky='SW', padx=pad, pady=pad)
         self.FixColors(self.shortcuts)
         self.row+=1
 
